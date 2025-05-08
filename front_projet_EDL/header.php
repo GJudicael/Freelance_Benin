29d98754ee6e62ff7d3b40d1f39e4dd7b7c14b2b
GBAGUIDI Judicael <moulero.gbaguidi@gmail.com>
2025-05-08 04:05:55 +0100

    Second envoi

--- front_projet_EDL/header.php
+++ front_projet_EDL/header.php
@@ -4,10 +4,11 @@
     <div class="container">
         
             <h4 class="text-secondary site fs-4 fw-bolder me-4"> FreeBenin </h4>
-           
             <ul class="navbar-nav">
                 <li class="nav-item"> <a href="#" class="navbar-brand fs-6"> Accueil</a> </li>
-                <li class="nav-item"> <a href="#" class="navbar-brand fs-6"> Effectuer une demande </a> </li>
+                
+                <li class="nav-item"> <a href="../HTML/Demande.html" class="navbar-brand fs-6"> Effectuer une demande </a> </li>
+                
                 <li class="nav-item"> <a href="#" class="navbar-brand fs-6" > Se déconnecter </a> </li>   
                 
             </ul>
@@ -21,12 +22,10 @@
             </form>
             <div>
                 <img src="348031e77ad618631ce86cc8aa1825c8.jpg" alt="photo" class=" rounded-circle me-2 ms-3" width="40px" height="40px"> 
-                <a href="#" class="navbar-brand fs-6"> Profile</a>
+                <a href="../HTML/Profile.html" class="navbar-brand fs-6"> Profile</a>
             </div>
         </div>
-    
-     
-    </div> 
+    </div>
 
 </nav>
 </header>
