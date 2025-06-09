965eff8780cbcd4b1cc69890a3c16804f5bf9ecd
Jarvis <esperoakando@gmail.com>
2025-06-09 18:01:48 +0100

    Ajout du header

--- messagerie/discussions.php
+++ messagerie/discussions.php
@@ -178,79 +178,83 @@ $conversations = $resultat->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
-    <div class="container-fluid d-flex px-0" id="wrapper">
-        <!-- Colonne de gauche : Liste des conversations -->
 
-        <div class="left border-3 border-end">
-            <h6 class="mt-2">Conversations</h6>
-            <hr class="mb-4">
-            <?php if (empty($conversations)) : ?>
-                <p>Aucune conversation entamée...</p>
-            <?php else: ?>
-                <?php foreach ($conversations as $conv) : ?>
-                    <a href="discussions.php?user_id=<?= $conv['id'] ?>" class="text-decoration-none text-black" id="conv">
-                        <div class="d-flex align-items-center p-2 rounded-3" id="conv">
-                            <img src="./../photo_profile/<?= $conv['photo'] ?>" alt="Photo de profil de <?= $conv['nom'] . ' ' . $conv['prenom'] ?>" class="rounded-circle" width="50" height="50">
-                            <div class="ms-3 w-100 infos_conv">
-                                <p class="mb-0 fs-6 fw-bolder"><?= $conv['nom'] . ' ' . $conv['prenom'] ?></p>
-                                <p class="mb-0"><?= formatterChaine($conv['message'], LONGUEUR_MESSAGE) ?></p>
-                                <span id="date" class="fs-6 text-black fw-light"><?= afficherDate($conv['created_at']) ?></span>
+    <?php require_once(__DIR__ . "/../front_projet_EDL/header.php") ?>
+    <main>
+        <div class="container-fluid d-flex px-0 mt-2" id="wrapper">
+            <!-- Colonne de gauche : Liste des conversations -->
+
+            <div class="left border-3 border-end">
+                <h6 class="mt-2">Conversations</h6>
+                <hr class="mb-4">
+                <?php if (empty($conversations)) : ?>
+                    <p>Aucune conversation entamée...</p>
+                <?php else: ?>
+                    <?php foreach ($conversations as $conv) : ?>
+                        <a href="discussions.php?user_id=<?= $conv['id'] ?>" class="text-decoration-none text-black" id="conv">
+                            <div class="d-flex align-items-center p-2 rounded-3" id="conv">
+                                <img src="./../photo_profile/<?= $conv['photo'] ?>" alt="Photo de profil de <?= $conv['nom'] . ' ' . $conv['prenom'] ?>" class="rounded-circle" width="50" height="50">
+                                <div class="ms-3 w-100 infos_conv">
+                                    <p class="mb-0 fs-6 fw-bolder"><?= $conv['nom'] . ' ' . $conv['prenom'] ?></p>
+                                    <p class="mb-0"><?= formatterChaine($conv['message'], LONGUEUR_MESSAGE) ?></p>
+                                    <span id="date" class="fs-6 text-black fw-light"><?= afficherDate($conv['created_at']) ?></span>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                <?php endforeach; ?>
-            <?php endif; ?>
-        </div>
+                        </a>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </div>
 
-        <!-- Colonne de droite : Affichage de la discussion -->
-        <div class="right d-flex flex-column <?= $receiver_id ? 'justify-content-between' : 'justify-content-center align-items-center' ?> ">
+            <!-- Colonne de droite : Affichage de la discussion -->
+            <div class="right d-flex flex-column <?= $receiver_id ? 'justify-content-between' : 'justify-content-center align-items-center' ?> ">
 
-            <?php if ($receiver_id) : ?>
-                <!-- Header -->
-                <div class="pb-2 d-flex align-items-center border-bottom border-3">
-                    <img src="./../photo_profile/<?= $infos_destinataire['photo'] ?>" alt="Photo de <?= $infos_destinataire['nom'] . ' ' . $infos_destinataire['prenom'] ?>" class="rounded-circle" width="40" height="40">
-                    <p class="mx-2 mb-0"><strong><?= $infos_destinataire['nom'] . ' ' . $infos_destinataire['prenom'] ?></strong> (<i><?= $infos_destinataire['nomDUtilisateur'] ?></i>)</p>
-                    <!-- <?php if (empty($discussion)) : ?>
+                <?php if ($receiver_id) : ?>
+                    <!-- Header -->
+                    <div class="pb-2 d-flex align-items-center border-bottom border-3">
+                        <img src="./../photo_profile/<?= $infos_destinataire['photo'] ?>" alt="Photo de <?= $infos_destinataire['nom'] . ' ' . $infos_destinataire['prenom'] ?>" class="rounded-circle" width="40" height="40">
+                        <p class="mx-2 mb-0"><strong><?= $infos_destinataire['nom'] . ' ' . $infos_destinataire['prenom'] ?></strong> (<i><?= $infos_destinataire['nomDUtilisateur'] ?></i>)</p>
+                        <!-- <?php if (empty($discussion)) : ?>
                         <a href="../front_projet_EDL/info_profile.php?id=<?= $selected_user_id ?>">Voir le profil</a>
                     <?php endif; ?> -->
-                </div>
-
-                <!-- Messages proprements dits -->
-
-                <div class="d-flex flex-column h-75 py-2 mb-3 overflow-y-scroll" id="messagesCont">
-                    <?php if (empty($discussion)) : ?>
-                        <!-- Rien à afficher -->
-                    <?php else: ?>
-                        <?php foreach ($discussion as $index => $msg) : ?>
-                            <?php
-                            if ($index != 0) {
-                                $message_precedent = $discussion[$index - 1];
-                                $meme_auteur = $message_precedent['sender_id'] == $msg['sender_id'] ? true : false;
-                            }
-                            $from_current_user = $msg['sender_id'] == $current_user_id;
-                            ?>
-
-                            <div class="mb-1 <?= $from_current_user ? 'from-me bg-primary text-white' : 'from-other bg-light' ?> message">
-                                <p class="py-2 px-4 mb-0 me-[20px]"><?= nl2br(htmlspecialchars($msg['message'])) ?></p>
-                            </div>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                </div>
-
-                <!-- Formulaire d'envoi -->
-                <form action="traitements/send_message.php" method="post" class="mb-3">
-                    <div class="d-flex">
-                        <textarea name="message" id="messageCont" class="form-control" placeholder="Saissisez votre message" rows="1"></textarea>
-                        <!-- <input type="text" name="message" id="message" class="form-control form-control-sm" placeholder="Saisissez votre message"> -->
-                        <button type="submit" class="btn btn-primary mx-2">Envoyer</button>
                     </div>
-                </form>
 
-            <?php else: ?>
-                <p>Aucune discussion n'a été sélectionnée.</p>
-            <?php endif; ?>
+                    <!-- Messages proprements dits -->
+
+                    <div class="d-flex flex-column h-75 py-2 mb-3 overflow-y-scroll" id="messagesCont">
+                        <?php if (empty($discussion)) : ?>
+                            <!-- Rien à afficher -->
+                        <?php else: ?>
+                            <?php foreach ($discussion as $index => $msg) : ?>
+                                <?php
+                                if ($index != 0) {
+                                    $message_precedent = $discussion[$index - 1];
+                                    $meme_auteur = $message_precedent['sender_id'] == $msg['sender_id'] ? true : false;
+                                }
+                                $from_current_user = $msg['sender_id'] == $current_user_id;
+                                ?>
+
+                                <div class="mb-1 <?= $from_current_user ? 'from-me bg-primary text-white' : 'from-other bg-light' ?> message">
+                                    <p class="py-2 px-4 mb-0 me-[20px]"><?= nl2br(htmlspecialchars($msg['message'])) ?></p>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+
+                    <!-- Formulaire d'envoi -->
+                    <form action="traitements/send_message.php" method="post" class="mb-3">
+                        <div class="d-flex">
+                            <textarea name="message" id="messageCont" class="form-control" placeholder="Saissisez votre message" rows="1"></textarea>
+                            <!-- <input type="text" name="message" id="message" class="form-control form-control-sm" placeholder="Saisissez votre message"> -->
+                            <button type="submit" class="btn btn-primary mx-2">Envoyer</button>
+                        </div>
+                    </form>
+
+                <?php else: ?>
+                    <p>Aucune discussion n'a été sélectionnée.</p>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
+    </main>
 
     <script>
         window.addEventListener('keydown', (event) => {
